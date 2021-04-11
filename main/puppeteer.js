3d9e4b89ddfea738eb1e8674bbf3f1f04736b3fa
hou503 <81844290+hou503@users.noreply.github.com>
2021-04-11 16:04:54 +0800

    Update puppeteer.js

--- main/puppeteer.js
+++ main/puppeteer.js
@@ -3,27 +3,25 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const sleep = require('../help/sleep');
 const {clearCookie, random} = require('../help/util');
 const options = require('../help/options');
-
+var EventEmitter = require('events').EventEmitter; 
+var event = new EventEmitter(); 
+event.setMaxListeners(200)
 puppeteer.use(StealthPlugin());
 
-const page = async ({ url, selector, callback }, showX) => {
+const page = async ({ url, selector, callback }, browser) => {
   console.log('start launch puppeteer ...', url, 'callback:', !!callback, 'selector:', selector)
-  options.headless = !!showX;
-  const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
   try{
     await clearCookie(browser);
     await page.goto(url, { timeout: 0, waitUntil: 'networkidle2' });
-    const randomTime = random(30, 50) * 10;
-    await sleep(randomTime);
-    console.log('load page down', url, randomTime);
+    console.log('load page down', url);
   } catch(error) {
     console.error('-------------------------------------no! error', error);
   }
   if (callback) {
     await callback({page, selector});
   }
-  const randomTime = random(20, 40) * 100 
+  const randomTime = random(40, 120) * 100
   await sleep(randomTime);
   const pages = await browser.pages();
   const urls = [];
@@ -31,18 +29,34 @@ const page = async ({ url, selector, callback }, showX) => {
     const page = pages[i];
     const url = page.url();
     urls.push(url)
-
   }
-  browser.close();
-  console.log(url, 'done~',urls.length, urls.toString(), randomTime);
+  page.close();
+  event.emit('next'); 
+  console.log(url, 'done~', urls.length, urls.toString(), randomTime);
 }
 
-const launch = async (lists, showX) => {
-  for (let i = 0; i < lists.length; i++) {
+const launch = async (lists, showX, tabs = 4) => {
+  options.headless = !!showX;
+  const browser = await puppeteer.launch(options);
+  let init = tabs;
+  event.on('next', async() => {
+    init = init + 1;
+    const list = lists[init];
+    if (init === lists.length) {
+      await sleep(10 * 1000);
+      browser.close();
+      console.log('~~~~~~~~~~~~~~~~~~task down~~~~~~~~~~~~~~~~~~~~~~~~~~');
+    } 
+    if (!!list) {
+      page({...list}, browser);
+    }
+  }); 
+  for (let i = 0; i < tabs; i++) {
     const list = lists[i];
-    await page({...list}, showX);
+    await sleep(1000);
+    page({...list}, browser);
   }
-  console.log('task down~');
+  
 }
 
 module.exports = launch;
