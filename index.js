a9d68e8937eed704522ca99b1b128bd1cbb39a58
hou503 <81844290+hou503@users.noreply.github.com>
2021-04-11 10:01:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const launch = require('./main/puppeteer');
-const { click } = require('./help/callbacks');
 
-const glamourshub = require('./config/glamourshub');
 const hawishub = require('./config/hawishub');
 
-const resolveData = ({ datas, selector, callback = click }) => datas.map(i => ({ url: i, selector, callback }))
+const resolveData = ({ datas, selector }) => datas.map(i => ({ url: i, selector }))
 
 const go = async () => {
   const args = process.argv.splice(2);
   const [ hideX ] = args;
+  const tabs = 6;
   const datas = [
     ...resolveData(hawishub),
-    ...resolveData(glamourshub),
   ];
-  launch(datas, hideX);
+  launch(datas, hideX, tabs);
 }
 
 go();
